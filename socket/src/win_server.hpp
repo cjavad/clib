9168639fd3bfd7ea8811913beb8793ada995904a
javadsm <javadshafique@hotmail.com>
2017-11-24 21:41:49 +0100

    Working on support for windown

---
+++ socket/src/win_server.hpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#pragma comment (lib, "Ws2_32.lib")
+#pragma comment (lib, "Mswsock.lib")
+#pragma comment (lib, "AdvApi32.lib")
