6ab3c670a1268cc325e2db313f3e4cb34881d215
javadsm <javadshafique@hotmail.com>
2017-11-25 22:10:07 +0100

    workinf

--- http/src/multi_http.cpp
+++ http/src/multi_http.cpp
@@ -2,13 +2,13 @@
 #include "param.hpp"
 #include "../../socket/src/unix_client.hpp"
 
-class http {
-    public:
-        std::string get(std::string url, std::string query_s);
-        std::string post(std::string url, std::string query_s);
+class HTTP {
+        public:
+         std::string get(std::string url, std::string query_s);
+         std::string post(std::string url, std::string query_s);
 };
 
-std::string http::get(std::string url, std::string query_s){
+std::string HTTP::get(std::string url, std::string query_s){
     Client c(url, 80, 2048);
     std::string request = "GET " + query_s + " HTTP/1.1";
     std::cout << request << std::endl;
@@ -16,7 +16,7 @@ std::string http::get(std::string url, std::string query_s){
     return c.recv(true);
 }
 
-std::string http::post(std::string url, std::string query_s){
+std::string HTTP::post(std::string url, std::string query_s){
     Client c(url, 80, 2048);
     std::string request = "POST " + query_s + " HTTP/1.1";
     std::cout << request << std::endl;
@@ -25,6 +25,6 @@ std::string http::post(std::string url, std::string query_s){
 }
 
 int main(){
-    http c;
-    std::cout << c.get("https://google.com", "/index.html") << std::endl;
+    HTTP http;
+    std::cout << http.get("https://google.com", "/index.html") << std::endl;
 }
