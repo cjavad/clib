0af86a1447ac02e0d89d6d99e82134196a07df0c
javadsm <javadshafique@hotmail.com>
2017-11-27 14:01:40 +0100

    Working on list

--- string/src/string.hpp
+++ string/src/string.hpp
@@ -1,10 +1,31 @@
+/*
+Clib string libary Copyrigth (C) Javad Shafique 2017
+*/
+
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cctype>
+#include <tuple>
 #include <vector>
 
+
+/*
+Strings class. funcs =>
+______________________
+[private] String::swap_case(char c); Swaps a char used in String::swapcase
+[public] String::join(std::vector<std::string>); Inspired by python takes vector of strings an adds them with a seperator (the string).
+[public] String::split(std::string sep); takes a seperator an splits the string by that seperator. returna a vector.
+[public] String::upper(); returns an all uppercase string.
+[public] String::lower(); returns an all lowercase string.
+[public] String::swapcase(); returns a string with swapped cases. uses String::swap_case(char c);
+*/
 class String {
     std::string self;
+    private:
+        //swap case of letters
+        static char swap_case(char c){if (std::isupper(c)) {return std::tolower(c);} else {return std::toupper(c);}}
+
     public:
         //constuctor
         String(std::string str);
@@ -15,6 +36,8 @@ class String {
         //quick'n dirty lowercase/uppercase functions
         std::string upper(){std::string str = self; std::transform(str.begin(), str.end(), str.begin(), ::toupper); return str;};
         std::string lower(){std::string str = self; std::transform(str.begin(), str.end(), str.begin(), ::tolower); return str;};
+        //and some swapcase stuff
+        std::string swapcase(){std::string str = self; std::transform(str.begin(), str.end(), str.begin(), this->swap_case); return str;}
 };
 
 /*
@@ -63,3 +86,52 @@ std::string String::join(std::vector<std::string> arr){
     }
     return out;
 }
+
+/*
+Basic_List class. functions =>
+______________________________
+a vector for each datatype we support
+a append for each datatype we support
+a get for each datatype we support
+
+a basic list system that powers List
+
+*/
+
+class Basic_List {
+    private:
+        //a bunch of vectors
+        std::vector<char> char_vec;
+        std::vector<int> int_vec;
+        std::vector<float> float_vec;
+        std::vector<std::string> str_vec;
+    public:
+        void append_c(char c){char_vec.push_back(c);};
+        void append_i(int i){int_vec.push_back(i);};
+        void append_f(float f){float_vec.push_back(f);};
+        void append_s(std::string s){str_vec.push_back(s);};
+
+        int size_c(){return char_vec.size();};
+        int size_i(){return int_vec.size();};
+        int size_f(){return float_vec.size();};
+        int size_s(){return str_vec.size();};
+
+        void pop_c(){return char_vec.pop_back();};
+        void pop_i(){return int_vec.pop_back();};
+        void pop_f(){return float_vec.pop_back();};
+        void pop_s(){return str_vec.pop_back();};
+
+        void clear_c(){return char_vec.clear();};
+        void clear_i(){return int_vec.clear();};
+        void clear_f(){return float_vec.clear();};
+        void clear_s(){return str_vec.clear();};
+
+        char get_c(int index){return char_vec[index];};
+        int get_i(int index){return int_vec[index];};
+        float get_f(int index){return float_vec[index];};
+        std::string get_s(int index){return str_vec[index];};
+};
+
+class List {
+
+};
--- string/tests/test.cc
+++ string/tests/test.cc
@@ -4,6 +4,8 @@
 
 int main(){
     std::string b = "Hello World LIFE";
+    Basic_List l;
+    l.append_s(b);
     String a(b);
     std::vector<std::string> v = a.split(" ");
     for (size_t i = 0; i < v.size(); i++)
@@ -14,5 +16,6 @@ int main(){
     String d("-");
     std::cout << d.join(v) << " " << v.size() << std::endl;
     std::cout << a.upper() << " " << a.lower() << std::endl;
+    std::cout << a.swapcase() << l.get_s(0) << std::endl;
     return 0;
 }
